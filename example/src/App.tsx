0fe5aca6bdecf3dc1f9d8c807a7ba4d54573aeda
Denis Erashov <d.erashov@vk.team>
2022-06-17 12:35:35 +0300

    Setup a demo

---
+++ example/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import SayHello from "crypto-wallet-link";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SayHello name={'asdf'} />
+    </div>
+  );
+}
+
+export default App;
