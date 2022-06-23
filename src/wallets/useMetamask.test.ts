bdacbcbec129e6048375f3a619609d78a3952f05
Gary Anikin <garyanikin@gmail.com>
2022-06-23 14:48:16 +0300

    useMetamask isAvailable status

--- src/wallets/useMetamask.test.ts
+++ src/wallets/useMetamask.test.ts
@@ -10,3 +10,22 @@ test('should return wallet id', () => {
     } = renderHook(() => useMetamask())
     expect(walletId).toBe(WalletId.metamask)
 })
+
+test('should be not available by defaut', () => {
+    const {
+        result: {
+            current: [, isAvailable],
+        },
+    } = renderHook(() => useMetamask())
+    expect(isAvailable).toBeFalsy()
+})
+
+test('should return availability', () => {
+    window.ethereum = { isMetamask: true }
+    const {
+        result: {
+            current: [, isAvailable],
+        },
+    } = renderHook(() => useMetamask())
+    expect(isAvailable).toBeTruthy()
+})
--- src/wallets/useMetamask.ts
+++ src/wallets/useMetamask.ts
@@ -2,8 +2,9 @@ import { WalletId } from './constants'
 
 function useMetamask() {
     const walletId = WalletId.metamask
+    const isAvailable = window?.ethereum?.isMetamask || false
 
-    return [walletId]
+    return [walletId, isAvailable]
 }
 
 export default useMetamask
