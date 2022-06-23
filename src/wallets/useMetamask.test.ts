aa47ccae97d42d0ab9fc654ea66aa63008a86088
Gary Anikin <garyanikin@gmail.com>
2022-06-23 14:42:46 +0300

    useMetamask returns proper wallet id

--- src/wallets/useMetamask.test.ts
+++ src/wallets/useMetamask.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react'
 import useMetamask from './useMetamask'
 import { WalletId } from './constants'
 
--- src/wallets/useMetamask.ts
+++ src/wallets/useMetamask.ts
@@ -1,5 +1,9 @@
-function useMetamask() {
+import { WalletId } from './constants'
 
+function useMetamask() {
+    const walletId = WalletId.metamask
+
+    return [walletId]
 }
 
 export default useMetamask
